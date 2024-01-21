12ae7d8c1959efab719a56082d7a1f053ddd40b3
nivicius <niviciusdev@gmail.com>
2024-01-21 04:46:20 -0300

    updating files

---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- options.lua
+
+-- Use actual tab characters instead of spaces
+vim.opt.expandtab = false
+
+-- Set the number of spaces for each tab
+vim.opt.tabstop = 4
+
+-- Set the number of spaces for automatic indentation
+vim.opt.shiftwidth = 4
+
+-- Enable automatic indentation
+vim.opt.autoindent = true
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  { "craftzdog/solarized-osaka.nvim" },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {},
+  },
+}
